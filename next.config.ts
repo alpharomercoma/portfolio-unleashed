678acdd5097d240710f53203c2359e69f15f5d55
Alpha Romer Coma <alpharomercoma@proton.me>
2024-12-13 14:05:42 +0800

    perf: optimize package imports in next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import("next").NextConfig} */
+const config = {
+  experimental: {
+    optimizePackageImports: ["react-icons", "lucide-react"],
+  },
 };
 
-export default nextConfig;
+export default config;
