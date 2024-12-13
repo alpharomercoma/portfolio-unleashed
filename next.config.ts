f1697059298a685087bcba67d8f17309edb591e4
Alpha Romer Coma <alpharomercoma@proton.me>
2024-12-13 14:55:57 +0800

    feat: add and config next-pwa

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,16 @@
+import { WithPWA } from "next-pwa";
+import nextPWA from "next-pwa";
+
+const withPWA: WithPWA = nextPWA({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+  scope: '/',
+  cacheId: 'alpha',
+  cleanupOutdatedCaches: true,
+});
+
 /** @type {import("next").NextConfig} */
 const config = {
   experimental: {
@@ -5,4 +18,4 @@ const config = {
   },
 };
 
-export default config;
+export default withPWA(config);
