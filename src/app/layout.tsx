9de41403d21f21129ec9d5e98340e9fd38a6c5bb
Alpha Romer Coma <alpharomercoma@proton.me>
2024-12-13 16:28:21 +0800

    feat: enhance metadata and add robots.txt for improved SEO and site visibility

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,10 +15,52 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   title: "Alpha Romer Coma | Portfolio",
-  description: "",
+  applicationName: "Alpha",
+  description:
+    "Building the change of tomorrow | I am Alpha, striving to transcend AI to sentience. Welcome to my portfolio.",
+  authors: { name: "Alpha Romer Coma", url: "https://alpharomer.vercel.app" },
+  category: "Technology",
+  classification: "Technology",
+  keywords: [
+    "Alpha Romer Coma",
+    "Portfolio",
+    "Technology",
+    "Software Engineering",
+    "Artificial Intelligence",
+    "Machine Learning",
+    "Cloud Computing",
+    "GitHub",
+    "GitHub Campus Expert",
+  ],
+  robots: {
+    follow: true,
+    index: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+  publisher: "Alpha Romer Coma",
+  twitter: {
+    card: "summary_large_image",
+    site: "@alpharomercoma",
+    creator: "@alpharomercoma",
+    images: "/cover.png",
+  },
+  openGraph: {
+    description: "Building the change of tomorrow | I am Alpha, striving to transcend AI to sentience. Welcome to my portfolio.",
+    title: "Alpha Romer Coma | Portfolio",
+    type: "website",
+    locale: "en_US",
+    url: "https://alpharomer.vercel.app",
+    siteName: "Alpha",
+    emails: "alpharomercoma@proton.me",
+    images: "/cover.png",
+  },
+  referrer: "no-referrer",
   appleWebApp: {
     title: "Alpha",
-  }
+  },
 };
 
 export default function RootLayout({
